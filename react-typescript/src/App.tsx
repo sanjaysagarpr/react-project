82b916d69bab56e3dd4d0cdf06fc7c3d86199ce0
sanjaysagarpr <sanjayprsanjaypr06431@gmail.com>
2023-12-19 19:46:41 +0530

    1

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+       <h1>sanjay pr</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
